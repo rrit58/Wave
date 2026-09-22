c885c2dc573aad816154c5279e1d535915126570
rrit58 <rohitrajind58@gmail.com>
2026-09-22 22:19:24 +0530

    minor fix

---
+++ backend/server/src/app.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import prisma from "./config/dbConfig.js";
+import userRoutes from "./routes/userRoute.js";
+import chatRoutes from "./routes/chatRoute.js";
+
+const app = express();
+const port = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route
+try {
+    app.get("/", (req, res) => {
+        res.send("Welcome to Wave Backend...🚀");
+    });
+} catch (error) {
+    console.log(error.message);
+}
+
+try {
+    app.use("/user", userRoutes);
+    app.use("/chat", chatRoutes);
+} catch (error) {
+    console.log(error.message);
+}
+
+try {
+    await prisma.$connect();
+    console.log("Database Connected Successfully ✅️");
+} catch (error) {
+    console.log("Database Connection Failed ❌️", error);
+}
+
+
+export default app;
--- backend/server/src/server.js
+++ backend/server/src/server.js
@@ -1,41 +1,8 @@
-import express from "express";
-import cors from "cors";
-import prisma from "./config/dbConfig.js";
-import userRoutes from "./routes/userRoute.js";
-import chatRoutes from "./routes/chatRoute.js";
+import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Route
-try {
-    app.get("/", (req, res) => {
-        res.send("Welcome to Wave Backend...🚀");
-    });
-} catch (error) {
-    console.log(error.message);
-}
-
-try {
-    app.use("/user", userRoutes);
-    app.use("/chat", chatRoutes);
-} catch (error) {
-    console.log(error.message);
-}
-
-try {
-    await prisma.$connect();
-    console.log("Database Connected Successfully ✅️");
-} catch (error) {
-    console.log("Database Connection Failed ❌️", error);
-}
-
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Auth & API Server is running on port ${port} 🚀`);
